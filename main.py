985d9c991746720e4d2de306160368cce940ff60
umerasif67 <umerasifbzn@gmail.edu.pk>
2026-06-11 12:51:46 -0700

    add paddle movement

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+import pygame
+
+pygame.init()
+
+# screen setup
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Elite Table Tennis")
+
+# game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))  # black background
+    pygame.display.flip()
+
+pygame.quit()
